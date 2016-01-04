5edfa62f48233e9b2b415aa4f6be4d46405095c9
Jack Hart <jhart@ep.com>
2016-01-04 11:49:54 -0800

    refactored middleware to separate file middleware.js

--- server.js
+++ server.js
@@ -2,26 +2,15 @@ var express = require('express');
 var app = express();
 var HTTP_PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + ' : ' + new Date().toString() + '  >>  ' + req.method + '  ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 // specify middleware up top or it will never run
-// app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About your thing...');
 });
 
-//console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.listen(HTTP_PORT, function () {
